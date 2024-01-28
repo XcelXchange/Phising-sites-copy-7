6c2bf1337c9196cc77bedb8b4c097942e91a5143
wise-king001 <wiseking.wisdom@gmail.com>
2024-01-28 19:35:14 +0100

    no

--- js/config.js
+++ js/config.js
@@ -10,16 +10,17 @@ const firebaseconfig1 = {
   }
   const firebaseApp1 = firebase.initializeApp(firebaseconfig1, "app1");
   const Primarydatabase = firebaseApp1.database();
-  const firebaseConfig2 = {
-    apiKey: "AIzaSyAIQMhJBufZ5fhzQ2JUQsC8O-FiEU5vOGQ",
-    authDomain: "sara-lance.firebaseapp.com",
-    databaseURL: "https://sara-lance-default-rtdb.firebaseio.com",
-    projectId: "sara-lance",
-    storageBucket: "sara-lance.appspot.com",
-    messagingSenderId: "949456759005",
-    appId: "1:949456759005:web:08d613dd1f8239815a9334",
-    measurementId: "G-1XC50CDJED"
-  }
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig2 = {
+  apiKey: "AIzaSyBzI5PP2zcvIHZmXb5rnWbEXazlw3e9SKM",
+  authDomain: "chris-2eb7e.firebaseapp.com",
+  databaseURL: "https://chris-2eb7e-default-rtdb.firebaseio.com",
+  projectId: "chris-2eb7e",
+  storageBucket: "chris-2eb7e.appspot.com",
+  messagingSenderId: "260301856742",
+  appId: "1:260301856742:web:8b053081a545453a7ec027",
+  measurementId: "G-RZLV6NZW7W"
+};
   const firebaseApp2 = firebase.initializeApp(firebaseConfig2, "app2");
   const Secondarydatabase = firebaseApp2.database();
   
